15e026ca6b6e55f86e694f5753fcf4a87681ca49
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-09-12 18:32:54 -0400

    Added route with path parameter
    Reads a single hero from database by identifier.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 class HeroBase(SQLModel):
@@ -46,3 +46,11 @@ def read_heroes():
     with Session(engine) as session:
         heroes = session.exec(select(Hero)).all()
         return heroes
+
+@app.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return hero
